bdb187bd9ec6ca4a327fcce0fc9c3d47ad5d8af9
Kiash254 <codewithkiash@gmail.com>
2022-09-09 00:50:25 +0300

    regsterd the model in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+admin.site.site_header = "Kiash Blog  Admin"
+admin.site.site_title = "Kiash Blog Admin Portal"
+admin.site.index_title = "Welcome to Kiash Blog Portal"
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','created_on','status')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+
+admin.site.register(Post, PostAdmin)
