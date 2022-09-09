97f87695926c646bc3cdd15ddf4dfbcd3bebea78
Kiash254 <codewithkiash@gmail.com>
2022-09-09 10:51:27 +0300

    added media files

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-
+from  .models import Author, Category, Post
 # Register your models here.
 admin.site.site_header = "Kiash Blog  Admin"
 admin.site.site_title = "Kiash Blog Admin Portal"
 admin.site.index_title = "Welcome to Kiash Blog Portal"
 
 
-# class PostAdmin(admin.ModelAdmin):
-#     list_display = ('title','slug','author','created_on','status')
-#     list_filter = ("status",)
-#     search_fields = ['title', 'content']
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Category)
