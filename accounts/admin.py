fcaeb776ca7271c5f13274b7ac7b71b914ca4251
Kiash254 <codewithkiash@gmail.com>
2022-09-15 17:22:24 +0300

    added models account in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Profile
+# Register your models here
 
-# Register your models here.
+admin.site.site_header = 'kiash Blog Admin Panel'
+admin.site.site_title = 'kiash Blog Admin Panel'
+admin.site.index_title = 'kiash Blog Admin Panel'
+
+admin.site.register(User)
+admin.site.register(Profile)
