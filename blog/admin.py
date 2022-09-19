ab28f20f7d802d050752ecbe1e707fa454cffb3e
Kiash254 <codewithkiash@gmail.com>
2022-09-19 22:05:04 +0300

    main

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,1 @@
 from django.contrib import admin
-from  .models import Author, Category, Post
-# Register your models here.
-admin.site.site_header = "Kiash Blog  Admin"
-admin.site.site_title = "Kiash Blog Admin Portal"
-admin.site.index_title = "Welcome to Kiash Blog Portal"
-
-
-
-admin.site.register(Post)
-admin.site.register(Author)
-admin.site.register(Category)
