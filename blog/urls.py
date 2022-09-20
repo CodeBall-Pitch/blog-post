e0d0b1611aa1996bac5f0e49fc3f40ec533813b2
Kiash254 <codewithkiash@gmail.com>
2022-09-20 12:35:04 +0300

    added some blog models

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from .views import home, about, category_post, allposts, post
+from .views import home
 app_name = 'blog'
 urlpatterns = [
     path('',home,name='home'),
-    path('post/<slug>/', post, name = 'post'),
-    path('about/', about,name = 'about' ),
-    path('post/<slug>/', post, name = 'post'),
-    path('posts/', allposts, name = 'allposts'),
 ]
